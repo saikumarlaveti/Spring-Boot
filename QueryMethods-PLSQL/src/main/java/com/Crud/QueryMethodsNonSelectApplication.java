2de6e2c87d03bc7886709afc0bf5180f8dfe2d46
saikumar <saikumarlaveti5061@gmail.com>
2025-03-04 08:15:49 +0530

    restored

---
+++ QueryMethods-PLSQL/src/main/java/com/Crud/QueryMethodsNonSelectApplication.java
@@ -0,0 +1,13 @@
+package com.Crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryMethodsNonSelectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryMethodsNonSelectApplication.class, args);
+	}
+
+}
